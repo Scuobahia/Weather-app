a089a6324286f9b936f789ded28d56363c54b45e
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-06-02 21:34:23 -0600

    JavaScript Start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-// alert("hello");
+var api = {
+    key: "73cf05e1772b6f32d41c086ccbb12510",
+    baseurl: "https://api.openweathermap.org/data/2.5/"
+}
+var searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    console.log(evt);
+    if (evt.keyCode == 13){
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
+function getResults (query) {
+    fetch (`${api.baseurl}weather?q=${query}&units=imperial&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }) .then(displayResults);
+}
+function displayResults (weather){
+    console.log(weather);
+  
+}
