38cc388b023a6d01d8b7cd989810a36caadd26e3
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-06-08 18:09:04 -0600

    Some JavaScript fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,49 @@
 var api = {
     key: "73cf05e1772b6f32d41c086ccbb12510",
     baseurl: "https://api.openweathermap.org/data/2.5/"
-} 
-var API_KEY = "73cf05e1772b6f32d41c086ccbb12510";
-getWeatherData()
-function getWeatherData (){
-    navigator.geolocation.getCurrentPosition((success) =>{
-        console.log(success);
-        let {latitude, longitude} = success.coords;
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`).then(res =>res.json()).then(data => {
-            console.log(data)
-        })
-})
+}
 
+var uvDataEl = document.getElementById('uvi');
+var API_KEY = "73cf05e1772b6f32d41c086ccbb12510";
+
+function getWeatherData(weather) {
+  
+        console.log(weather);
+        let latitude = weather.coord.lat;
+        let longitude = weather.coord.lon;
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=hourly,minutely&appid=${API_KEY}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.current.uvi)
+                let uv = document.querySelector('#uvi');
+                uv.innerHTML = `${data.current.uvi}`
+                console.log(data)
+                let temp =document.querySelector('.current-2 .temp-2');
+                temp.innerHTML = `${data.daily[1].temp.day} <span>°F</span>`;
+                
+            })
+}
 var searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
-
 function setQuery(evt) {
+
     console.log(evt);
-    if (evt.keyCode == 13){
+    if (evt.keyCode == 13) {
         getResults(searchbox.value);
         console.log(searchbox.value);
     }
 }
-function getResults (query) {
-    fetch (`${api.baseurl}weather?q=${query}&units=imperial&APPID=${api.key}`)
-    .then(weather => {
-        return weather.json();
-    }) .then(displayResults);
+function getResults(query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(function (weather) {
+            getWeatherData(weather)
+            console.log("hello")
+            displayResults(weather)
+        });
 }
-function displayResults (weather){
+function displayResults(weather) {
     console.log(weather);
     let city = document.querySelector('.location .city');
     city.innerHTML = `${weather.name}, ${weather.sys.country}`;
@@ -43,7 +57,7 @@ function displayResults (weather){
 
     let weather_el = document.querySelector(`.current .weather`);
     weather_el.innerHTML = weather.weather[0].main;
-    
+
     let hilow = document.querySelector('.hi-low');
     hilow.innerHTML = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
 
@@ -55,4 +69,6 @@ function displayResults (weather){
 
     let feel = document.querySelector('.feel');
     feel.innerHTML = `${Math.round(weather.main.feels_like)} <span>°F</span>`;
-}}
+
+   
+}
