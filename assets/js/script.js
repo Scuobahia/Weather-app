1a304693f81aa7e0361a5f4cbb0cc82c01dc7e84
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-06-08 23:27:35 -0600

    weather forecast working

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,6 +48,51 @@ function getWeatherData(weather) {
                 
                 let uvi3 = document.querySelector('.current-3 .uvi-3');
                 uvi3.innerHTML = `${data.daily[2].uvi}`;
+                //Day 3 
+                let temp4 = document.querySelector('.current-4 .temp-4');
+                temp4.innerHTML = `${Math.round(data.daily[3].temp.day)} <span>°F</span>`;
+
+                let now4 = document.querySelector('.current-4 .now-4');
+                now4.innerHTML = `${data.daily[3].weather[0].main}`
+
+                let feel4 = document.querySelector('.current-4 .feel-4');
+                feel4.innerHTML = `${Math.round(data.daily[3].feels_like.day)} <span>°F</span>`;
+
+                let humid4 = document.querySelector('.current-4 .humid-4');
+                humid4.innerHTML = `${data.daily[3].humidity}<span>%</span>`
+                
+                let uvi4 = document.querySelector('.current-4 .uvi-4');
+                uvi4.innerHTML = `${data.daily[3].uvi}`;
+                //Day 4
+                let temp5 = document.querySelector('.current-5 .temp-5');
+                temp5.innerHTML = `${Math.round(data.daily[4].temp.day)} <span>°F</span>`;
+
+                let now5 = document.querySelector('.current-5 .now-5');
+                now5.innerHTML = `${data.daily[4].weather[0].main}`
+
+                let feel5 = document.querySelector('.current-5 .feel-5');
+                feel5.innerHTML = `${Math.round(data.daily[4].feels_like.day)} <span>°F</span>`;
+
+                let humid5 = document.querySelector('.current-5 .humid-5');
+                humid5.innerHTML = `${data.daily[4].humidity}<span>%</span>`
+                
+                let uvi5 = document.querySelector('.current-5 .uvi-5');
+                uvi5.innerHTML = `${data.daily[4].uvi}`;
+                //Day 5
+                let temp6 = document.querySelector('.current-6 .temp-6');
+                temp6.innerHTML = `${Math.round(data.daily[5].temp.day)} <span>°F</span>`;
+
+                let now6 = document.querySelector('.current-6 .now-6');
+                now6.innerHTML = `${data.daily[5].weather[0].main}`
+
+                let feel6 = document.querySelector('.current-6 .feel-6');
+                feel6.innerHTML = `${Math.round(data.daily[5].feels_like.day)} <span>°F</span>`;
+
+                let humid6 = document.querySelector('.current-6 .humid-6');
+                humid6.innerHTML = `${data.daily[5].humidity}<span>%</span>`
+                
+                let uvi6 = document.querySelector('.current-6 .uvi-6');
+                uvi6.innerHTML = `${data.daily[5].uvi}`;
             })
 }
 var searchbox = document.querySelector('.search-box');
